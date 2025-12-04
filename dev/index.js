2a00f83deac2151c1fb950f9f2ffe57c6ef17e40
Andy <andy.10lin@hotmail.com>
2025-12-04 16:21:07 -0500

    Adds organization to files and adds background colour

--- dev/index.js
+++
@@ -1,6 +0,0 @@
-let num;
-
-document.getElementById("submitButton").onclick = function() {
-    num = document.getElementById("numberField").value;
-    document.getElementById("result").textContent = num;
-}
