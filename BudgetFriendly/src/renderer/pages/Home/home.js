5293c85e0cee6dd209aa219f1e75b76fbc42a788
Andy <andy.10lin@hotmail.com>
2025-12-29 23:10:47 -0500

    Add functionality to sidebar buttons and update .prettierrc.yaml to include semi-colons in js files

--- BudgetFriendly/src/renderer/pages/Home/home.js
+++ BudgetFriendly/src/renderer/pages/Home/home.js
@@ -1,6 +1,23 @@
-const hamburger = document.querySelector(".hamburger");
-const sidebar = document.querySelector(".sidebar");
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
 
-hamburger.addEventListener("click", () => {
-    sidebar.classList.toggle("open");
+const sidebarLabels = sidebar.querySelectorAll('.sidebar-labels > .label');
+const pages = document.querySelectorAll('.page');
+
+hamburger.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
+});
+
+sidebar.addEventListener('click', (event) => {
+    const mouseClick = event.target;
+  if (mouseClick.classList.contains('label')) {
+    pages.forEach((page) => {
+      const key = mouseClick.dataset.page;
+      if (page.dataset.page === key) {
+        page.classList.remove('hide-display');
+        return;
+      }
+      page.classList.add('hide-display');
+    });
+  }
 });
