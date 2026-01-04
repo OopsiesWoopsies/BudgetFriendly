0c4bdb08f121164c8f5539cce70692eaffe5f2b1
Andy <andy.10lin@hotmail.com>
2026-01-03 23:14:55 -0500

    Add functionality to the card header buttons and calendar

--- BudgetFriendly/src/renderer/pages/Budgeting/sheet.js
+++ BudgetFriendly/src/renderer/pages/Budgeting/sheet.js
@@ -1,15 +1,22 @@
 const daysDiv = document.getElementById('days');
+const calendar = document.querySelector('.calendar');
+const calendarBody = document.querySelector('.calendar-body');
+const calendarHeaderTitle = document.querySelector('.title');
+const monthSelection = document.querySelector('.month-selection');
+const daySelection = document.querySelector('.day-selection');
+const cardHeader = document.querySelector('.card-header');
+const budgetSheet = document.querySelector('.budget-sheet');
 
-const currDate = new Date();
-const calendar = Array.from({ length: 5 }, () => Array(7).fill(null));
-
-const year = currDate.getFullYear();
-const month = currDate.getMonth();
-
-const daysInMonth = new Date(year, month + 1, 0).getDate();
-const firstDay = new Date(year, month, 1).getDay();
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
 
 function createCalendar() {
+  let calendarArr = Array.from({ length: 5 }, () => Array(7).fill(null));
+
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  const firstDay = new Date(year, month, 1).getDay();
+
   let day = 1;
   let week = 0;
 
@@ -29,7 +36,7 @@ function createCalendar() {
       week++;
       i = 0;
     }
-    calendar[week][i] = day;
+    calendarArr[week][i] = day;
     createDiv(daysDiv, day);
   }
 
@@ -41,4 +48,58 @@ function createCalendar() {
   }
 }
 
-createCalendar();
+cardHeader.addEventListener('click', (event) => {
+  if (!monthSelection.classList.contains('display-none')) {
+    if (event.target.id === 'left-arrow') {
+      year--;
+      calendarHeaderTitle.textContent = year;
+    } else if (event.target.id === 'right-arrow') {
+      year++;
+      calendarHeaderTitle.textContent = year;
+    }
+  }
+  if (!daySelection.classList.contains('display-none')) {
+    if (event.target.id === 'left-arrow') {
+      if (--month < 0) {
+        month = 11;
+        year--;
+      }
+      monthName = document.getElementById(month).textContent;
+      calendarHeaderTitle.textContent = `${monthName}, ${year}`;
+      date = new Date(year, month, 1);
+      calendarBody.innerHTML = '';
+      createCalendar();
+    } else if (event.target.id === 'right-arrow') {
+      if (++month > 11) {
+        month = 0;
+        year++;
+      }
+      monthName = document.getElementById(month).textContent;
+      calendarHeaderTitle.textContent = `${monthName}, ${year}`;
+      date = new Date(year, month, 1);
+      calendarBody.innerHTML = '';
+      createCalendar();
+    } else if (event.target.classList.contains('title')) {
+      monthSelection.classList.remove('display-none');
+      daySelection.classList.add('display-none');
+      calendarBody.innerHTML = '';
+      calendarHeaderTitle.textContent = year;
+    }
+  }
+});
+
+calendar.addEventListener('click', (event) => {
+  const selectedCell = event.target.closest('.month-name');
+  if (!selectedCell) {
+    return;
+  }
+  if (!monthSelection.classList.contains('display-none')) {
+    month = Number(event.target.id);
+    date = new Date(year, month, 1);
+    createCalendar();
+    monthSelection.classList.add('display-none');
+    daySelection.classList.remove('display-none');
+    calendarHeaderTitle.textContent = `${selectedCell.textContent}, ${year}`;
+  } else if (!daySelection.classList.contains('display-none')) {
+  }
+});
