03af89f7c5a2d8243549a54b8922a7106b91aa10
Andy <andy.10lin@hotmail.com>
2026-02-09 17:08:20 -0500

    Add budgeting options to budget creation popup and submit and cancel buttons

--- src/renderer/pages/home/js/home.js
+++ src/renderer/pages/home/js/home.js
@@ -1,12 +1,5 @@
 import { initSettingsListeners } from '../../budgeting/js/settings.js';
 
-const addBugetButton = document.querySelector('.add-budget-button');
-const creationPopUp = document.getElementById('settings');
-
-export function initHomeListeners() {
-  addBugetButton.addEventListener('click', () => {
-    creationPopUp.showModal();
-  });
-}
+export function initHomeListeners() {}
 
 initSettingsListeners();
